af37a54d6c78503eac64b48e7b647ab3a7460fc0
EricBroyles <eric.christopher.broyles@gmail.com>
2025-05-31 01:41:04 -0400

    finished build phase, script names

---
+++ Scripts/User Interface/BuildPhaseUserInterface.gd
@@ -0,0 +1,18 @@
+extends Node
+
+
+##this just displays the infrmation stored inside of challengeGame class (want to get stuff onreadu)
+##it needs to handle the various buttons displaying stuff
+##when you hit the chekmark button -> this calls ChallengeGameUserInterFace (UI layer) which has the method to swap between control layouts.
+
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
